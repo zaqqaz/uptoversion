cecfba3ae275740165d86402d656aca0789f2cfc
Denis Artyuhovich <i@denis.by>
2020-08-05 18:02:30 +0100

    Build code a bit prettier

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,28 @@
 import { execSync } from 'child_process';
-import fs from "fs";
-import { Octokit } from "@octokit/rest";
+import fs from 'fs';
+import { Octokit } from '@octokit/rest';
 
 enum PackageManager {
     Npm = 'npm',
     Yarn = 'yarn',
 }
 
-const randomString = (length: number) => [...Array(length)].map(() => (~~(Math.random() * 36)).toString(36)).join('');
+const randomString = (length: number) =>
+    [...Array(length)]
+        .map(() => (~~(Math.random() * 36)).toString(36))
+        .join('');
 
 function execSyncFromProjectFolder(cmd: string) {
     execSync(`cd ./${root} && ${cmd}`);
 }
 
 function checkoutRepo(repoName: string) {
-    const cmd = `git clone git@github.com:${repoName}.git ${root}`
+    const cmd = `git clone git@github.com:${repoName}.git ${root}`;
     execSync(cmd);
 }
 
 function createBranch(branchName: string) {
-    [
-        `git checkout -b ${branchName}`
-    ].forEach(execSyncFromProjectFolder);
+    [`git checkout -b ${branchName}`].forEach(execSyncFromProjectFolder);
 }
 
 function updatedDependency(
@@ -39,7 +40,7 @@ function updatedDependency(
 function commit(packageName: string, version: string) {
     [
         `git add .`,
-        `git commit -m "Update ${packageName} to v.${version}"`
+        `git commit -m "Update ${packageName} to v.${version}"`,
     ].forEach(execSyncFromProjectFolder);
 }
 
@@ -48,13 +49,11 @@ async function createPR(
     packageName: string,
     version: string,
     repoName: string,
-    baseBranch: string = "master"
+    baseBranch: string = 'master'
 ) {
-    [
-        `git push -u origin ${branchName}`,
-    ].forEach(execSyncFromProjectFolder);
+    [`git push -u origin ${branchName}`].forEach(execSyncFromProjectFolder);
 
-    const [owner, repo] = repoName.split('/')
+    const [owner, repo] = repoName.split('/');
     await octokit.pulls.create({
         owner,
         repo,
@@ -77,21 +76,26 @@ async function run() {
     createBranch(branchName);
     updatedDependency(PackageManager.Npm, packageName, packageVersion);
     commit(packageName, packageVersion);
-    await createPR(branchName, packageName, packageVersion, repoName, baseBranch);
+    await createPR(
+        branchName,
+        packageName,
+        packageVersion,
+        repoName,
+        baseBranch
+    );
 }
 
 const octokit = new Octokit({
-    auth: `token ${process.env.GITHUB_TOKEN}`
+    auth: `token ${process.env.GITHUB_TOKEN}`,
 });
 
-const root = fs.mkdtempSync("tmp");
+const root = fs.mkdtempSync('tmp');
 
 run()
     .then(() => {
         fs.rmdirSync(root, { recursive: true });
     })
-    .catch(e => {
+    .catch((e) => {
         console.error(e);
         fs.rmdirSync(root, { recursive: true });
     });
-
